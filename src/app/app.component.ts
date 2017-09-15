a5c01f922ebca315c662cbee700c28988515a8f6
steven.guiheux <steven.guiheux@corp.ovh.com>
2017-09-15 16:50:06 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {ModalTemplate, SuiModalService, TemplateModalConfig} from 'ng2-semantic-ui';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    @ViewChild('modalTemplate')
+    public modalTemplate: ModalTemplate<IContext, string, string>;
+
+    title = 'app';
+
+    constructor(public modalService: SuiModalService) {
+    }
+
+    public open(dynamicContent: string = "Example") {
+        const config = new TemplateModalConfig<IContext, string, string>(this.modalTemplate);
+
+        config.closeResult = "closed!";
+        config.context = {data: dynamicContent};
+
+        this.modalService
+            .open(config)
+            .onApprove(result => {
+                console.log('approved')
+            })
+            .onDeny(result => {
+                console.log('denied')
+            });
+    }
+
+
+    public globalConfig: TemplateModalConfig<IContext, string, string>;
+
+    public open2(dynamicContent: string = "Example") {
+        this.globalConfig = new TemplateModalConfig<IContext, string, string>(this.modalTemplate);
+        this.modalService
+            .open(this.globalConfig)
+            .onApprove(result => {
+                console.log('approved')
+            })
+            .onDeny(result => {
+                console.log('denied')
+            });
+    }
+}
+
+export interface IContext {
+    data: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SuiModule} from 'ng2-semantic-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
